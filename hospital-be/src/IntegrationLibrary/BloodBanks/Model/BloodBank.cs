66730451e063e39408e826506fa163e2ac3a03a2
Sanja Petrović <sanjica.petrovic7@gmail.com>
2022-10-25 01:08:19 +0200

    Added migration

--- hospital-be/src/IntegrationLibrary/BloodBanks/Model/BloodBank.cs
+++ hospital-be/src/IntegrationLibrary/BloodBanks/Model/BloodBank.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IntegrationLibrary.BloodBanks.Model
 {
+    [Table("blood_banks")]
     public class BloodBank
     {
         public Guid Id { get; set; }
