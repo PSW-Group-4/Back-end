0844f5e1e138e5dfc9185e2c2bf320ea96288d48
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2022-10-25 22:10:33 +0200

    Removed unnecessary classes

--- hospital-be/src/HospitalLibrary/RoomsAndEqipment/Model/Room.cs
+++ hospital-be/src/HospitalLibrary/RoomsAndEqipment/Model/Room.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace HospitalLibrary.RoomsAndEqipment.Model
 {
-    public class Room
+    public abstract class Room
     {
         public Guid Id {get; set;}
         public String Name {get; set;}
