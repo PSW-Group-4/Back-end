abdd70d25ef8a94404e7c696deefbeddf8ac47e1
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2022-10-25 12:35:53 +0200

    Added API Key Generation Util class

---
+++ hospital-be/src/IntegrationLibrary/Utilities/ApiKeyGeneration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationLibrary.Utilities
+{
+    public class ApiKeyGeneration
+    {
+        public static String generateKey(List<String> keyList)
+        {
+            String keyString;
+            do
+            {
+                var key = new byte[32];
+                using (var generator = RandomNumberGenerator.Create())
+                    generator.GetBytes(key);
+                keyString = Convert.ToBase64String(key);
+
+            } while (!isUnique(keyList, keyString));
+
+            return keyString;
+        }
+
+        private static Boolean isUnique(List<String> keyList, String newKey)
+        {
+            if(keyList == null || keyList.Count == 0)
+            {
+                return true;
+            }
+            else 
+            {
+                return keyList.Contains(newKey);
+            }
+
+        }
+    }
+}
