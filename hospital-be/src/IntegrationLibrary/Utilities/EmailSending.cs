ff04a2ff9b04d0ff574c2efd68c62be55687c9f1
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2022-10-25 16:39:13 +0200

    Added Email creation and sending utilities

---
+++ hospital-be/src/IntegrationLibrary/Utilities/EmailSending.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+namespace IntegrationLibrary.Utilities
+{
+    public class EmailSending
+    {
+        private static String _senderName = "PSW Integrations";
+        private static String _email = "psw.integrations.g4@gmail.com";
+        private static String _password = "mcezencvkdktyarh";
+
+        public static MimeMessage createTxtEmail(String recipientName, String recipientEmail,String subject,String emailText)
+        {
+            var message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress(_senderName, _email));
+            message.To.Add(new MailboxAddress(recipientName, recipientEmail));
+
+            message.Subject = subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = @"" + emailText
+            };
+            return message;
+        }
+        public static void sendEmail(MimeMessage message)
+        {
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                client.Authenticate(_email, _password);
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
