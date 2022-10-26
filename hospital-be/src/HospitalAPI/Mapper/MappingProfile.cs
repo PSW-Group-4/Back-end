450d03f095ec9fd498d7b4813bd23667754298db
stefaN-N-N <stefanlepsanovic@gmail.com>
2022-10-26 18:33:24 +0200

    added room controller and dto

--- hospital-be/src/HospitalAPI/Mapper/MappingProfile.cs
+++ hospital-be/src/HospitalAPI/Mapper/MappingProfile.cs
@@ -3,9 +3,11 @@ using HospitalAPI.Controllers.Dtos.Address;
 using HospitalAPI.Controllers.Dtos.Doctor;
 using HospitalAPI.Controllers.Dtos.Patient;
 using HospitalAPI.Controllers.Dtos.Person;
+using HospitalAPI.Controllers.Dtos.Rooms;
 using HospitalLibrary.Core.Model;
 using HospitalLibrary.Doctors.Model;
 using HospitalLibrary.Patients.Model;
+using HospitalLibrary.RoomsAndEqipment.Model;
 
 namespace HospitalAPI.Mapper
 {
@@ -19,6 +21,8 @@ namespace HospitalAPI.Mapper
                 .IncludeBase<PersonRequestDto, Person>();
             CreateMap<DoctorRequestDto, Doctor>()
                 .IncludeBase<PersonRequestDto, Person>();
+            CreateMap<RoomRequestDto, Room>();
+
         }
     }
 }
