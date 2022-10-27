b5638b3a2e9264f2c175c0f1a30d075554b28d56
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2022-10-27 18:16:23 +0200

    Added RoomMaps sql

---
+++ hospital-be/DbScripts/Maps/FloorMaps.sql
@@ -0,0 +1,3 @@
+INSERT INTO public."FloorMaps"(
+	"Id", "FloorId", "CoordinateX", "CoordinateY", "Height", "Width")
+	VALUES ('fc47af3d-5a41-402b-8863-9aae827e9008', '9845c31d-e22a-48f6-b9ef-f8fc13e5f21c', 100, 100, 500, 500);
---
+++ hospital-be/DbScripts/Maps/RoomMaps.sql
@@ -0,0 +1,6 @@
+INSERT INTO public."RoomMaps"("Id", "RoomId", "CoordinateX", "CoordinateY", "Height", "Width")
+	VALUES ('1dd897c6-56a0-4b9c-be76-c5cfd0ccfea0', '9ae3255d-261f-472f-a961-7f2e7d05d95c', 100, 100, 70, 70);
+INSERT INTO public."RoomMaps"("Id", "RoomId", "CoordinateX", "CoordinateY", "Height", "Width")
+	VALUES ('4c8b0af2-9936-46a3-9eaf-002840685f64', '8dd731ee-197f-40a8-a5e1-845662b0c0cd', 170, 100, 70, 70);
+INSERT INTO public."RoomMaps"("Id", "RoomId", "CoordinateX", "CoordinateY", "Height", "Width")
+	VALUES ('c2c47c06-4403-4f77-8c70-ed570e2a8812', 'd4857133-ef89-4e5e-865f-b49c83ecec23', 100, 170, 70, 70);
