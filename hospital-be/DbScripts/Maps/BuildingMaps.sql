9fd6bdb477ef58cc3783ef6598c57bd69eecf733
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2022-10-27 17:39:37 +0200

    Added sql script file for building maps - Still needs to be edited later, mostly testing connections

---
+++ hospital-be/DbScripts/Maps/BuildingMaps.sql
@@ -0,0 +1,3 @@
+INSERT INTO public."BuildingMaps"(
+	"Id", "BuildingId", "CoordinateX", "CoordinateY", "Height", "Width")
+	VALUES ('bf9fefb6-d1d8-47f9-b97f-50e099d63f90', '4c08ff1f-0227-4a3c-93db-dcd865a1069b', 100, 100, 800, 800);
