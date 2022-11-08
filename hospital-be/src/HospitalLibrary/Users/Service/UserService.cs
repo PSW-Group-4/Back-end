e9127c2ec1437ae4c509f909cad79edc326beee3
Strahinja Eraković <karte32114@gmail.com>
2022-11-08 22:03:49 +0100

    User controller added

--- hospital-be/src/HospitalLibrary/Users/Service/UserService.cs
+++ hospital-be/src/HospitalLibrary/Users/Service/UserService.cs
@@ -32,6 +32,16 @@ namespace HospitalLibrary.Users.Service
             return _userRepository.Create(user);
         }
 
+        public User RegisterPatient(User user, Guid patientId)
+        {
+            user.PersonId = patientId;
+            user.Role = UserRole.PATIENT;
+            user.IsAccountActive = false;
+            user.IsBlocked = false;
+
+            return _userRepository.Create(user);
+        }
+
         public User Update(User user)
         {
             return _userRepository.Update(user);
