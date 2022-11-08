7b8942c0a3ed52719ba243dd4c773828ce8fcce8
Strahinja Eraković <karte32114@gmail.com>
2022-11-08 18:42:02 +0100

    Added User model

---
+++ hospital-be/src/HospitalLibrary/Allergies/Allergie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Allergies
+{
+    public class Allergie
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
