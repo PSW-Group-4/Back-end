8f1784cbe1ebe95c1bd25ed5ddfdcf63b4b189f1
strale15 <karte32114@gmail.com>
2022-11-09 12:06:37 +0100

    Testing new scripts

---
+++ hospital-be/DbScripts/Skripte2/DbScripts/Patients.sql
@@ -0,0 +1,4 @@
+INSERT INTO public."Patients" ("Id","Lbo","Blocked","Name","Surname","Birthdate","Gender","AddressId","Jmbg","Email","PhoneNumber") VALUES
+	 ('0c75e30a-1b93-4eb1-b601-5a1422e86636','41234123414',false,'Marko','Markovic','2000-01-03 00:00:00',0,'de3ebdd7-d0da-4646-9bd1-e04df2c562ad','837198219312','marko@gmail.com','06412332111'),
+	 ('5a365cb2-d52a-4dc4-96a5-d37af79da08c','123412341234',false,'Stefan','Stefanovic','1995-05-03 00:00:00',0,'be3135bf-47fb-454a-9e64-6af7a0fc838c','49371947194192','stefan@gmail.com','06315124125'),
+	 ('5bdd96ec-9b9f-4c77-b74d-4425b8c65032','48923589092',false,'Milica','Mirandic','1988-08-11 17:02:05.107',1,'081a6c25-719c-4424-a032-6255ff98c28c','1108881028410','milica@gmail.com','06487610921');
