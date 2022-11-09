662b71d1aae7c6b6b298d357ec33417cca4cf417
strale15 <karte32114@gmail.com>
2022-11-09 12:06:37 +0100

    Testing new scripts

---
+++ hospital-be/DbScripts/Skripte2/DbScripts/Buildings.sql
@@ -0,0 +1,3 @@
+insert into Public."Buildings" ("Id", "Name") values 
+    ('4c08ff1f-0227-4a3c-93db-dcd865a1069b', 'justo nec condimentum neque sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros'),
+    ('6f8f6c66-a869-4715-8229-95df705418a6', 'justo nec condimentum neque sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros');
