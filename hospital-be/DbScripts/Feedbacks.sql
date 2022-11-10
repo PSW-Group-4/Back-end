77f7971dbcacb964ac3f15e17ece97011cd03277
stojanovic00 <al.stojanovic0@gmail.com>
2022-11-10 06:53:25 +0100

    Fixed dbReset.py

--- hospital-be/DbScripts/Feedbacks.sql
+++ hospital-be/DbScripts/Feedbacks.sql
@@ -1,5 +1,5 @@
 INSERT INTO public."Feedbacks" ("Id","Text","IsAnonimous","IsDesiredPublic","Status","PatientId","Date") VALUES
-	 ('da243b20-2d31-458f-ab28-66f2d6653149','Very good',true,true,2,'0c75e30a-1b93-4eb1-b601-5a1422e86636','2022-10-29 19:06:26.026728'),
-	 ('d27eff98-5e81-420d-83a3-6175be4939b7','Could be better',false,false,1,'0c75e30a-1b93-4eb1-b601-5a1422e86636','2022-10-29 19:08:33.852363'),
-	 ('3e75f50a-6cf1-4818-8f82-445c89c4e430','Excellent',false,true,0,'0c75e30a-1b93-4eb1-b601-5a1422e86636','2022-10-29 19:07:45.829214'),
-	 ('aec658b3-bcd5-43db-8668-8361363923ef','Doctor Strahinja chopped my finger and laughed',true,false,0,'0c75e30a-1b93-4eb1-b601-5a1422e86636','2022-10-29 19:07:12.273741');
+	 ('da243b20-2d31-458f-ab28-66f2d6653149','Very good',true,true,2,'501cb78e-d157-49b5-9093-f87833f5ab42','2022-10-29 19:06:26.026728'),
+	 ('d27eff98-5e81-420d-83a3-6175be4939b7','Could be better',false,false,1,'7306ce59-2c2e-4b8c-8dc6-6b1977a237fa','2022-10-29 19:08:33.852363'),
+	 ('3e75f50a-6cf1-4818-8f82-445c89c4e430','Excellent',false,true,0,'11f66ba5-eabb-4956-bdd4-960b83201d0b','2022-10-29 19:07:45.829214'),
+	 ('aec658b3-bcd5-43db-8668-8361363923ef','Doctor Strahinja chopped my finger and laughed',true,false,0,'13984a82-bdac-42f5-b856-d76df3f85f9b','2022-10-29 19:07:12.273741');
--- hospital-be/dbReset.py
+++ hospital-be/dbReset.py
@@ -2,17 +2,75 @@ import os
 
 destFile = open('dbResetScript.sql', 'w')
 
-#Clears all tables
-for fileName in os.listdir("DbScripts"):
-    print('delete from "' + fileName[:len(fileName)-4] +'";', file=destFile)
+#Clears all tables - I ovo mora rucno...
+print('delete from "RoomMaps";', file=destFile)
+
+print('delete from "FloorMaps";', file=destFile)
+
+print('delete from "BuildingMaps";', file=destFile)
+
+print('delete from "Appointments";', file=destFile)
+
+print('delete from "Users";', file=destFile)
+
+print('delete from "Feedbacks";', file=destFile)
+
+print('delete from "PatientAllergies";', file=destFile)
+
+print('delete from "Patients";', file=destFile)
+
+print('delete from "Doctors";', file=destFile)
+
+print('delete from "Rooms";', file=destFile)
+
+print('delete from "Floors";', file=destFile)
+
+print('delete from "Buildings";', file=destFile)
+
+print('delete from "Allergies";', file=destFile)
+
+print('delete from "Addresses";', file=destFile)
+
 
 #Merges scripts
 #Bitno je da dodas prvo skripte od kojih zavise strani kljucevi
-with open('DbScripts/Addresses.sql', 'r') as f:
+with open('DbScripts/Addresses.sql', 'r', encoding='utf-8-sig') as f:
     print(f.read(), file=destFile)
 
-with open('DbScripts/Patients.sql', 'r') as f:
+with open('DbScripts/Allergies.sql', 'r', encoding='utf-8-sig') as f:
     print(f.read(), file=destFile)
 
-with open('DbScripts/Feedbacks.sql', 'r') as f:
+with open('DbScripts/Buildings.sql', 'r', encoding='utf-8-sig') as f:
+    print(f.read(), file=destFile)
+
+with open('DbScripts/Floors.sql', 'r', encoding='utf-8-sig') as f:
+    print(f.read(), file=destFile)
+
+with open('DbScripts/Rooms.sql', 'r', encoding='utf-8-sig') as f:
+    print(f.read(), file=destFile)
+
+with open('DbScripts/Doctors.sql', 'r', encoding='utf-8-sig') as f:
+    print(f.read(), file=destFile)
+with open('DbScripts/Patients.sql', 'r', encoding='utf-8-sig') as f:
+    print(f.read(), file=destFile)
+
+with open('DbScripts/PatientAllergies.sql', 'r', encoding='utf-8-sig') as f:
+    print(f.read(), file=destFile)
+
+with open('DbScripts/Feedbacks.sql', 'r', encoding='utf-8-sig') as f:
+    print(f.read(), file=destFile)
+
+with open('DbScripts/Users.sql', 'r', encoding='utf-8-sig') as f:
+    print(f.read(), file=destFile)
+
+with open('DbScripts/Appointments.sql', 'r', encoding='utf-8-sig') as f:
+    print(f.read(), file=destFile)
+
+with open('DbScripts/BuildingMaps.sql', 'r', encoding='utf-8-sig') as f:
+    print(f.read(), file=destFile)
+
+with open('DbScripts/FloorMaps.sql', 'r', encoding='utf-8-sig') as f:
+    print(f.read(), file=destFile)
+
+with open('DbScripts/RoomMaps.sql', 'r', encoding='utf-8-sig') as f:
     print(f.read(), file=destFile)
