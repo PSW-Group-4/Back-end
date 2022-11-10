fff18a1b2d06399ed8ae487f1a4017099ead38db
stojanovic00 <al.stojanovic0@gmail.com>
2022-11-10 06:53:25 +0100

    Fixed dbReset.py

--- hospital-be/DbScripts/Skripte2/DbScripts/Feedbacks.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public."Feedbacks" ("Id","Text","IsAnonimous","IsDesiredPublic","Status","PatientId","Date") VALUES
-	 ('da243b20-2d31-458f-ab28-66f2d6653149','Very good',true,true,2,'0c75e30a-1b93-4eb1-b601-5a1422e86636','2022-10-29 19:06:26.026728'),
-	 ('d27eff98-5e81-420d-83a3-6175be4939b7','Could be better',false,false,1,'0c75e30a-1b93-4eb1-b601-5a1422e86636','2022-10-29 19:08:33.852363'),
-	 ('3e75f50a-6cf1-4818-8f82-445c89c4e430','Excellent',false,true,0,'0c75e30a-1b93-4eb1-b601-5a1422e86636','2022-10-29 19:07:45.829214'),
-	 ('aec658b3-bcd5-43db-8668-8361363923ef','Doctor Strahinja chopped my finger and laughed',true,false,0,'0c75e30a-1b93-4eb1-b601-5a1422e86636','2022-10-29 19:07:12.273741');
