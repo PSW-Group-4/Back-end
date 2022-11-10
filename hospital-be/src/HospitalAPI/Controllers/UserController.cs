e794615eff9556c206cf818790f64f883ba38146
Aleksandar <82851295+stojanovic00@users.noreply.github.com>
2022-11-10 15:55:36 +0100

    Revert "Moved authetication logic to dedicated services"
    This reverts commit 649be02f08c21a83b4d71f5af61f40f01d7a84c7.

--- hospital-be/src/HospitalAPI/Controllers/UserController.cs
+++ hospital-be/src/HospitalAPI/Controllers/UserController.cs
@@ -1,15 +1,24 @@
 ﻿using AutoMapper;
+using HospitalAPI.Dtos.Feedback;
 using HospitalAPI.Dtos.User;
 using HospitalLibrary.Core.Model;
 using HospitalLibrary.Core.Service;
 using HospitalLibrary.Exceptions;
+using HospitalLibrary.Feedbacks.Service;
 using HospitalLibrary.Patients.Model;
 using HospitalLibrary.Patients.Service;
 using HospitalLibrary.Users.Model;
 using HospitalLibrary.Users.Service;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using HospitalLibrary.Core.Service.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
 
 namespace HospitalAPI.Controllers
 {
@@ -18,15 +27,19 @@ namespace HospitalAPI.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly IAddressService _addressService;
         private readonly IPatientService _patientService;
         private readonly IMapper _mapper;
+        private readonly IConfiguration _config;
 
         public UserController(IUserService userService, IAddressService addressService,
-            IPatientService patientService, IMapper mapper, IJwtService jwtService)
+            IPatientService patientService, IMapper mapper, IConfiguration config)
         {
             _userService = userService;
+            _addressService = addressService;
             _patientService = patientService;
             _mapper = mapper;
+            _config = config;
         }
 
         //POST api/user/registerPatient
@@ -63,7 +76,8 @@ namespace HospitalAPI.Controllers
         {
             try
             {
-                var token = _userService.Authenticate(userLogin.Username, userLogin.Password);
+                var user = _userService.Authenticate(userLogin.Username, userLogin.Password);
+                var token = Generate(user);
                 return Ok(token);
             }
             catch(NotFoundException)
@@ -75,5 +89,43 @@ namespace HospitalAPI.Controllers
                 return Unauthorized("Bad password");
             }
         }
+
+        private string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Audience"],
+              claims,
+              expires: DateTime.Now.AddMinutes(30),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        //Otprilike ovako nekako mozes iz tokena da izvuces claims npr username
+        /*private UserLoginDto GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+
+                return new UserLoginDto
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    //Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }*/
     }
 }
--- hospital-be/src/HospitalLibrary/Users/Service/UserService.cs
+++ hospital-be/src/HospitalLibrary/Users/Service/UserService.cs
@@ -2,7 +2,9 @@
 using HospitalLibrary.Users.Repository;
 using System;
 using System.Collections.Generic;
-using HospitalLibrary.Core.Service.Interfaces;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using HospitalLibrary.Exceptions;
 using NotFoundException = IntegrationLibrary.Exceptions.NotFoundException;
 
@@ -11,12 +13,10 @@ namespace HospitalLibrary.Users.Service
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
-        private readonly IJwtService _jwtService;
 
-        public UserService(IUserRepository userRepository, IJwtService jwtService)
+        public UserService(IUserRepository userRepository)
         {
             _userRepository = userRepository;
-            _jwtService = jwtService;
         }
 
         public IEnumerable<User> GetAll()
@@ -44,7 +44,7 @@ namespace HospitalLibrary.Users.Service
             return _userRepository.Create(user);
         }
 
-        public string Authenticate(string username, string password)
+        public User Authenticate(string username, string password)
         {
             User user = _userRepository.GetByUsername(username);
             if (user == null)
@@ -57,7 +57,7 @@ namespace HospitalLibrary.Users.Service
                 throw new BadPasswordException();
             }
             
-            return _jwtService.GenerateToken(user);
+            return user;
         }
 
         public User Update(User user)
