b87cddb531359297ee2cb08ea0a9dd57754bd9c3
Sanja Petrović <sanjica.petrovic7@gmail.com>
2022-11-13 21:56:27 +0100

    Base integration test

---
+++ hospital-be/src/TestIntegrationApp/Setup/BaseIntegrationTest.cs
@@ -0,0 +1,20 @@
+﻿using IntegrationAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestIntegrationApp.Setup
+{
+    public class BaseIntegrationTest : IClassFixture<TestDatabaseFactory<Startup>>
+    {
+        protected TestDatabaseFactory<Startup> Factory { get; }
+
+        public BaseIntegrationTest(TestDatabaseFactory<Startup> factory)
+        {
+            Factory = factory;
+        }
+    }
+}
