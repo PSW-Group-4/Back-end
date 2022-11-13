3a1bb81b68ff7b7d0dd6cb9d29de406ae10ca54d
Sanja Petrović <sanjica.petrovic7@gmail.com>
2022-11-13 22:03:20 +0100

    Simple test

---
+++ hospital-be/src/IntegrationAPI/Controllers/NewsController.cs
@@ -0,0 +1,28 @@
+﻿using IntegrationLibrary.BloodBankNews.Model;
+using IntegrationLibrary.BloodBankNews.Service;
+using IntegrationLibrary.BloodBanks.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace IntegrationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly INewsService _service;
+
+        public NewsController(INewsService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            IEnumerable<News> news = _service.GetAll();
+            return Ok(news);
+        }
+    }
+}
---
+++ hospital-be/src/TestIntegrationApp/IntegrationTesting/NewsTests.cs
@@ -0,0 +1,37 @@
+﻿using IntegrationAPI;
+using IntegrationAPI.Controllers;
+using IntegrationLibrary.BloodBankNews.Model;
+using IntegrationLibrary.BloodBankNews.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestIntegrationApp.Setup;
+using Xunit;
+
+namespace TestIntegrationApp.IntegrationTesting
+{
+    public class NewsTests : BaseIntegrationTest
+    {
+        public NewsTests(TestDatabaseFactory<Startup> factory) : base(factory) { }
+
+        private static NewsController SetupController(IServiceScope scope)
+        {
+            return new NewsController(scope.ServiceProvider.GetRequiredService<INewsService>());
+        }
+
+        [Fact]
+        public void Retrieves_all_news()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var controller = SetupController(scope);
+
+            var result = ((OkObjectResult)controller.GetAll())?.Value as IEnumerable<News>;
+
+            Assert.NotNull(result);
+        }
+    }
+}
