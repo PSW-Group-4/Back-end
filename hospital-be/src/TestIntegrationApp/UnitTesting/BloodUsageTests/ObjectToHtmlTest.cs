5759ecc52d3618e0335742faa14b81035ab35b88
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2022-11-15 18:43:11 +0100

    Html gen test Refactor

--- hospital-be/src/TestIntegrationApp/UnitTesting/BloodUsageTests/ObjectToHtmlTest.cs
+++ hospital-be/src/TestIntegrationApp/UnitTesting/BloodUsageTests/ObjectToHtmlTest.cs
@@ -14,6 +14,40 @@ namespace TestIntegrationApp.UnitTesting.BloodUsageTests
         [Fact]
         public void Checks_if_object_is_converted()
         {
+            BloodBank bloodBank = GenerateTestBloodBank();
+            ReportConfiguration repConfig = GenerateReportConfiguration(bloodBank);
+            BloodUsageReport bloodUsageReport = GenerateBloodUsageReport(bloodBank, repConfig);
+
+            String html = bloodUsageReport.GenerateHtml();
+
+            string testHtml = System.IO.File.ReadAllText(@"/UnitTesting/BloodUsageTests/BRHtmlTest.txt");
+            bool htmlsAreEqual = (html == testHtml);
+            htmlsAreEqual.ShouldBe(true);
+        }
+
+        private BloodUsageReport GenerateBloodUsageReport(BloodBank bloodBank, ReportConfiguration repConfig)
+        {
+            BloodUsageReport bloodUsageReport = new BloodUsageReport();
+            bloodUsageReport.Id = Guid.NewGuid();
+            bloodUsageReport.BloodBank = bloodBank;
+            bloodUsageReport.ReportConfiguration = repConfig;
+            bloodUsageReport.BloodUsage = this.generateTestBloodUsage();
+            bloodUsageReport.timeOfCreation = new DateTime(1970, 1, 1, 0, 0, 0);
+            return bloodUsageReport;
+        }
+
+        private static ReportConfiguration GenerateReportConfiguration(BloodBank bloodBank)
+        {
+            ReportConfiguration repConfig = new ReportConfiguration();
+            repConfig.Id = Guid.NewGuid();
+            repConfig.BloodBank = bloodBank;
+            repConfig.RequestFrequency = ReportFrequencyTitles.MONTHLY;
+            repConfig.ActiveStatus = true;
+            return repConfig;
+        }
+
+        private static BloodBank GenerateTestBloodBank()
+        {
             BloodBank bloodBank = new BloodBank();
             bloodBank.Id = Guid.NewGuid();
             bloodBank.Name = "TestBank";
@@ -21,7 +55,10 @@ namespace TestIntegrationApp.UnitTesting.BloodUsageTests
             bloodBank.EmailAddress = "psw.integrations.g4@gmail.com";
             bloodBank.Password = "1234";
             bloodBank.ApiKey = "takll0fr5iKieWBYIwwIYcD_xeAKF_D9yguE5PyPPLI8";
-
+            return bloodBank;
+        }
+        private List<BloodUsage> generateTestBloodUsage()
+        {
             BloodUsage usageApos = new BloodUsage();
             usageApos.Id = Guid.NewGuid();
             usageApos.type = BloodType.A;
@@ -34,28 +71,10 @@ namespace TestIntegrationApp.UnitTesting.BloodUsageTests
             usage0neg.rHFactor = RHFactor.NEGATIVE;
             usage0neg.milliliters = 350;
 
-            ReportConfiguration repConfig = new ReportConfiguration();
-            repConfig.Id = Guid.NewGuid();
-            repConfig.BloodBank = bloodBank;
-            repConfig.RequestFrequency = ReportFrequencyTitles.MONTHLY;
-            repConfig.ActiveStatus = true;
-
-            BloodUsageReport bloodUsageReport = new BloodUsageReport();
-            bloodUsageReport.Id = Guid.NewGuid();
-            bloodUsageReport.BloodBank = bloodBank;
-            bloodUsageReport.ReportConfiguration = repConfig;
-            bloodUsageReport.BloodUsage = new List<BloodUsage>();
-            bloodUsageReport.BloodUsage.Add(usage0neg);
-            bloodUsageReport.BloodUsage.Add(usageApos);
-            bloodUsageReport.timeOfCreation = new DateTime(1970, 1, 1, 0, 0, 0);
-
-            String html = bloodUsageReport.GenerateHtml();
-
-            string testHtml = System.IO.File.ReadAllText(@"/UnitTesting/BloodUsageTests/BRHtmlTest.txt");
-
-            bool htmlsAreEqual = (html == testHtml);
-
-            htmlsAreEqual.ShouldBe(true);
+            List<BloodUsage> retVal = new List<BloodUsage>();
+            retVal.Add(usageApos);
+            retVal.Add(usage0neg);
+            return retVal;
         }
     }
 }
