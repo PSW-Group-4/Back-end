0b9db7228a64835171ce547024d46b254d9bd83f
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2022-11-15 19:52:33 +0100

    Feature Html to Pdf convertor test & init.

---
+++ hospital-be/src/IntegrationLibrary/Utilities/HtmlToPdfConvertor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationLibrary.Utilities
+{
+    public class HtmlToPdfConvertor
+    {
+        public static String Convert(String html)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
