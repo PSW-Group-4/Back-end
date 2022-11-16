14030dac3bd06a394507c66e75a6419f23eb8e19
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2022-11-16 11:27:20 +0100

    Feature/usage reports gen (#53)
    * Refactor: moved classes related to reports
    
    * Wrote html generation Unit test.
    
    Wrote Unit test.
    Provided test html file.
    Added placeholder method to BloodUsageReport.cs
    
    * Html gen test Refactor
    
    * Added generateHtml method and some minor tweeks
    
    * Feature Html to Pdf convertor test & init.
    
    * Feature Html to Pdf converter

---
+++ hospital-be/src/TestIntegrationApp/UnitTesting/BloodUsageTests/HtmToSavedPdfTest.cs
@@ -0,0 +1,26 @@
+﻿using IntegrationLibrary.Utilities;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestIntegrationApp.UnitTesting.BloodUsageTests
+{
+    public class HtmToSavedPdfTest
+    {
+        [Fact]
+        public void Checks_if_pdf_exists()
+        {
+            string testHtml = System.IO.File.ReadAllText(Directory.GetCurrentDirectory().ToString() + @"./UnitTesting/BloodUsageTests/BRHtmlTest.txt");
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).ToString() + Path.DirectorySeparatorChar + "Reports" + Path.DirectorySeparatorChar;
+            string pdfPath = HtmlToPdfConvertor.Convert(testHtml, path,"TestBank");
+
+            bool exists = System.IO.File.Exists(pdfPath);
+            exists.ShouldBe(true);
+        }
+    }
+}
