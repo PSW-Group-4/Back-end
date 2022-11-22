9ec15ca32699cec15a6f0aa1509b1d599217e8ca
MiskLG <misklg283@gmail.com>
2022-11-22 14:27:14 +0100

    Fixed some database bugs and added migration

---
+++ hospital-be/DbScripts/RoomSchedules.sql
@@ -0,0 +1,3 @@
+INSERT INTO public."RoomSchedules" ("Id","RoomId","DateTime","IsDone","Duration") VALUES 
+                    ('fe02b9df-ed7f-4270-adf1-4f4d171d9dcb','9ae3255d-261f-472f-a961-7f2e7d05d95c','2022-11-04 00:00:00',true,30),
+                    ('3eef41c0-da48-4542-bc3a-36754929a2f7','9ae3255d-261f-472f-a961-7f2e7d05d95c','2022-12-12 00:00:00',false,30);
