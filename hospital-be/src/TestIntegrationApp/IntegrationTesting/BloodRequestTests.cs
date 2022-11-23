72caf4ddfa9d7a2abe00934454daff2b9f813ce0
BogdanDavinic <davinic.ra153.2019@uns.ac.rs>
2022-11-23 20:24:58 +0100

    committing tests for blood requests

--- hospital-be/src/TestIntegrationApp/IntegrationTesting/BloodRequestTests.cs
+++ hospital-be/src/TestIntegrationApp/IntegrationTesting/BloodRequestTests.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using IntegrationAPI;
 using IntegrationAPI.Controllers;
+using IntegrationAPI.Dtos.BloodRequests;
+using IntegrationLibrary.BloodBanks.Model;
 using IntegrationLibrary.BloodRequests.Model;
 using IntegrationLibrary.BloodRequests.Repository;
 using IntegrationLibrary.BloodRequests.Service;
@@ -35,6 +37,41 @@ namespace TestIntegrationApp.IntegrationTesting
             var controller = SetupController(scope);
             var result = ((OkObjectResult)controller.GetAll())?.Value as IEnumerable<BloodRequest>;
             Assert.NotNull(result);
+        }
+        [Fact]
+        public void Retrieves_not_approved()
+        {
+            bool success = true;
+            using var scope = Factory.Services.CreateScope();
+            var controller = SetupController(scope);
+            var result = ((OkObjectResult)controller.GetUnapproved())?.Value as IEnumerable<BloodRequest>;
+            if (result != null)
+                foreach (BloodRequest request in result)
+                {
+                    success = success && !request.IsApproved;
+                }
+            else success = false;
+            Assert.True(success);
+        }
+        [Fact]
+        public void Update()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var controller = SetupController(scope);
+            BloodRequestsCreateDto bloodRequest = new BloodRequestsCreateDto
+            {
+                BloodType = BloodType.A,
+                RHFactor = RHFactor.NEGATIVE,
+                ReasonsWhyBloodIsNeeded = "Reason",
+                BloodAmountInMilliliters = 100.00,
+                DateTime = DateTime.Now,
+            };
+            var create = controller.Create(bloodRequest);
+            var request = (((OkObjectResult)controller.GetAll())?.Value as IEnumerable<BloodRequestEditDto>).First();
+            request.BloodAmountInMilliliters = 200.00;
+            var result = ((OkObjectResult)controller.Update(request))?.Value as BloodRequestEditDto;
+            Assert.True(result.BloodAmountInMilliliters == 200.00);
+
         }
        
     }
