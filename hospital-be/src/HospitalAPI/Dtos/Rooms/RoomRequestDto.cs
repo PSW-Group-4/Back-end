3343b1d2c18775d219c47782a6e5bd7fbc82163e
stefaN-N-N <stefanlepsanovic@gmail.com>
2022-11-24 23:22:02 +0100

    room request dto fix

--- hospital-be/src/HospitalAPI/Dtos/Rooms/RoomRequestDto.cs
+++ hospital-be/src/HospitalAPI/Dtos/Rooms/RoomRequestDto.cs
@@ -4,7 +4,7 @@ namespace HospitalAPI.Dtos.Rooms
 {
     public class RoomRequestDto
     {
-        public Guid Id { get; set; }
+        //public Guid Id { get; set; }
         public String Name { get; set; }
         public int Number { get; set; }
         public String Description { get; set; }
