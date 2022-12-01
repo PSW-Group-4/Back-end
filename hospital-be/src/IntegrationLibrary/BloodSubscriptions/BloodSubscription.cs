f0b7c5d1106e34cbdb9afe6d3ba874c68e7641da
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2022-12-01 11:40:57 +0100

    Init

---
+++ hospital-be/src/IntegrationLibrary/BloodSubscriptions/BloodSubscription.cs
@@ -0,0 +1,43 @@
+﻿using IntegrationLibrary.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationLibrary.BloodSubscriptions
+{
+    public class BloodSubscription : Entity
+    {
+        public List<BloodType> BloodTypes 
+        {
+            get
+            {
+                return new List<BloodType>(this.BloodTypes);
+            }
+            private set { }
+        }
+        public string BloodBankName { get; private set; }
+        
+        public BloodSubscription()
+        {
+            BloodTypes = new List<BloodType>();
+        }
+        public BloodSubscription(Guid id, string bloodBankName)
+        {
+            Id = id;
+            BloodBankName = bloodBankName;
+            BloodTypes = new List<BloodType>();
+        }
+
+        public void addBloodType(BloodType type)
+        {
+
+        }
+
+        public void removeBloodType(BloodType type)
+        {
+
+        }
+    }
+}
