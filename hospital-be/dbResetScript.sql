a03d60053c03c1bc7286ac617cee78789af0110f
DusanVukovic3 <vukovic.ra16.2019@uns.ac.rs>
2022-12-04 14:27:06 +0100

    dbResetUpdate

--- hospital-be/dbResetScript.sql
+++ hospital-be/dbResetScript.sql
@@ -663,3 +663,7 @@ INSERT INTO public."RoomMaps"("Id", "RoomId", "CoordinateX", "CoordinateY", "Hei
 INSERT INTO public."Admissions" ("Id","PatientId","Reason","RoomId","arrivalDate") VALUES
 	 ('1412c639-c5e1-47a1-b29b-1fe925536612','11942a9c-850e-489a-8434-98704889d8ed','Povisen secer pa mora da se oporavi','54c6de76-bc71-4615-8c73-4ebf9546444f','2022-11-11 11:11:00'),
 	 ('15f4c4e0-08b2-44ef-9e1b-0f14164d59d2','c1db258b-3034-4ba5-98bd-4f68137bac94','Srcani zastoj u prednjoj komori','d4857133-ef89-4e5e-865f-b49c83ecec23','2022-09-09 10:10:00');
+
+INSERT INTO public."Vacations" ("Id", "DoctorId", "DateStart", "DateEnd", "Reason", "Urgent", "VacationStatus", "DeniedRequestReason") 
+		values 	('030d8222-259f-4995-899c-fc6aa7d6a053','6444dff2-fcd7-44b0-90cb-19326b7ad49d', '2022-11-04 00:00:00', '2022-11-05 05:00:00', 'Zenim se', true, 1, 'Ne moze ove nedelje'),
+		   	('54cacecd-6d49-40cb-ad60-d0b95bcbf7ba','6444dff2-fcd7-44b0-90cb-19326b7ad49d', '2022-12-20 00:00:00', '2022-12-28 16:00:00', 'Zenim se drugi put', false, 2, 'Moze')
