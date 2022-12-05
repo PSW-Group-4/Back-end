395c1299e440742b81e3c0d950245e4c5218eb0a
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2022-12-05 16:41:24 +0100

    Urgent and possibly all requests render init
    Commented out scheduled sender we might not need it.

---
+++ hospital-be/src/IntegrationAPI/Communications/Producer/BloodSubscription/BloodRequestProducer.cs
@@ -0,0 +1,54 @@
+﻿using IntegrationLibrary.BloodRequests.Service;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IntegrationAPI.Communications.Producer.BloodSubscription
+{
+    public class BloodRequestProducer : IHostedService
+    {
+        private readonly string _topic = "blood.subscriptions.topic";
+ 
+        public IServiceScopeFactory ServiceScopeFactory;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                using (IServiceScope scope = ServiceScopeFactory.CreateScope())
+                {
+                    IProducer producer = scope.ServiceProvider.GetRequiredService<IProducer>();
+                    IBloodRequestService _requestService = scope.ServiceProvider.GetService<IBloodRequestService>();
+
+                    foreach (var requests in _requestService.GetAll()) // change this 
+                    {
+                        try
+                        {
+                            // convert to DTO
+                            // serialise and send
+                            //producer.Send(JsonSerializer.Serialize(dto), _topic);
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine(ex.ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
