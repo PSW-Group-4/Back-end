ac377f75da841a52399219e031af48987f60634e
Petar Popovic <93181074+Petarpop202@users.noreply.github.com>
2022-12-09 16:13:24 +0100

    available termins for medical appointment

--- hospital-be/src/HospitalLibrary/MedicalAppointments/Service/MedicalAppointmentService.cs
+++ hospital-be/src/HospitalLibrary/MedicalAppointments/Service/MedicalAppointmentService.cs
@@ -4,6 +4,10 @@ using HospitalLibrary.Doctors.Service;
 using System;
 using System.Collections.Generic;
 using HospitalLibrary.Core.Service;
+using HospitalLibrary.Core.Model;
+using HospitalLibrary.Patients.Service;
+using HospitalLibrary.Patients.Model;
+using Microsoft.VisualBasic;
 
 namespace HospitalLibrary.Appointments.Service
 {
@@ -11,16 +15,18 @@ namespace HospitalLibrary.Appointments.Service
     {
         private readonly IMedicalAppointmentRepository _medicalAppointmentRepository;
         private readonly IDoctorService _doctorService;
+        private readonly IPatientService _patientService;
 
-        public MedicalAppointmentService(IMedicalAppointmentRepository medicalAppointmentRepository, IDoctorService doctorService)
+        public MedicalAppointmentService(IMedicalAppointmentRepository medicalAppointmentRepository, IDoctorService doctorService, IPatientService patientService)
         {
             _medicalAppointmentRepository = medicalAppointmentRepository;
             _doctorService = doctorService;
+            _patientService = patientService;
         }
 
         public IEnumerable<MedicalAppointment> GetAll()
         {
-            MedicalAppointmentService appointmentService = new MedicalAppointmentService(_medicalAppointmentRepository, _doctorService);
+            //MedicalAppointmentService appointmentService = new MedicalAppointmentService(_medicalAppointmentRepository, _doctorService);
             //appointmentService.CheckIfAppointmentIsDone();
             return _medicalAppointmentRepository.GetAll();
         }
@@ -45,5 +51,31 @@ namespace HospitalLibrary.Appointments.Service
             _medicalAppointmentRepository.Delete(appointmentId);
         }
 
+        public bool IsPatientFree(Guid patientId,DateRange rangeTime)
+        {
+            IEnumerable<MedicalAppointment> patientAppointments = GetAllPatientAppointments(patientId);
+            foreach (MedicalAppointment patientAppointment in patientAppointments)
+            {
+                if (patientAppointment.DateRange.OverlapsWith(rangeTime))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public IEnumerable<MedicalAppointment> GetAllPatientAppointments(Guid patientId)
+        {
+            IEnumerable<MedicalAppointment> medicalAppointments = _medicalAppointmentRepository.GetAll();
+            List<MedicalAppointment> patientAppointments =  new List<MedicalAppointment>();
+            foreach (MedicalAppointment appointment in medicalAppointments)
+            {
+                if (appointment.PatientId.Equals(patientId)){
+                    patientAppointments.Add(appointment);
+                }
+            }
+            return patientAppointments;
+        }
     }
 }
