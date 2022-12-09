1376aa0336fe75aa207329f225831e08e13fd0cb
DaniloBulatovic <bulatovic.ra159.2019@uns.ac.rs>
2022-12-09 01:42:29 +0100

    Small Report Dto and Repo fix

--- hospital-be/src/HospitalAPI/Dtos/Report/ReportRequestDto.cs
+++ hospital-be/src/HospitalAPI/Dtos/Report/ReportRequestDto.cs
@@ -1,3 +1,5 @@
+using HospitalAPI.Dtos.Prescription;
+using HospitalAPI.Dtos.Symptom;
 using System;
 using System.Collections.Generic;
 
@@ -5,12 +7,11 @@ namespace HospitalAPI.Dtos.Report
 {
     public class ReportRequestDto
     {
-        public Guid Id { get; set; }
         public Guid PatientId { get; set; }
         public Guid DoctorId { get; set; }
         public string Text { get; set; }
         public List<HospitalLibrary.Symptoms.Model.Symptom> Symptoms { get; set; }
-        public List<HospitalLibrary.Prescriptions.Model.Prescription> Prescriptions { get; set; }
+        public List<PrescriptionRequestDto> Prescriptions { get; set; }
         public DateTime DateTime { get; set; }
     }
 }
