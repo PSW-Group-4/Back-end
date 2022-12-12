0bac816f98a59f5c8702f33e6642f55e55aa1c41
Đorđe <the2922000@gmail.com>
2022-12-12 23:57:58 +0100

    Consilium migration added

--- hospital-be/src/HospitalLibrary/Migrations/20221212225549_Consilium.Designer.cs
+++ hospital-be/src/HospitalLibrary/Migrations/20221212225549_Consilium.Designer.cs
@@ -6,15 +6,17 @@ using HospitalLibrary.Settings;
 using HospitalLibrary.Users.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace HospitalLibrary.Migrations
 {
     [DbContext(typeof(HospitalDbContext))]
-    partial class HospitalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221212225549_Consilium")]
+    partial class Consilium
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
