8d31e40173c0b2d5feb2c5d2ce48dba6f3a7136b
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2022-12-12 14:05:35 +0100

    Init

---
+++ hospital-be/src/IntegrationLibrary/ManagerBloodRequests/Repository/ManagerRequestRepository.cs
@@ -0,0 +1,39 @@
+﻿using IntegrationLibrary.ManagerBloodRequests.Model;
+using IntegrationLibrary.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationLibrary.ManagerBloodRequests.Repository
+{
+    public class ManagerRequestRepository : IManagerRequestRepository
+    {
+        private readonly IntegrationDbContext _context;
+
+        public ManagerRequestRepository(IntegrationDbContext context)
+        {
+            _context = context;
+        }
+        public ManagerRequest Create(ManagerRequest bloodRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ManagerRequest> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ManagerRequest GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ManagerRequest Update(ManagerRequest bloodRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ hospital-be/src/IntegrationLibrary/ManagerBloodRequests/Service/ManagerRequestService.cs
@@ -0,0 +1,39 @@
+﻿using IntegrationLibrary.ManagerBloodRequests.Model;
+using IntegrationLibrary.ManagerBloodRequests.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationLibrary.ManagerBloodRequests.Service
+{
+    public class ManagerRequestService : IManagerRequestService
+    {
+        private readonly IManagerRequestRepository _repository;
+
+        public ManagerRequestService(IManagerRequestRepository repository)
+        {
+           _repository = repository;
+        }
+        public ManagerRequest Create(ManagerRequest bloodRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ManagerRequest> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ManagerRequest GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ManagerRequest Update(ManagerRequest bloodRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
