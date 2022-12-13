64ab99f9ffb7557ee0679c16f881806eacb6c247
MiskLG <misklg283@gmail.com>
2022-12-14 00:28:28 +0100

    Added migration

--- hospital-be/src/HospitalLibrary/Migrations/20221213224411_map-location-changes.Designer.cs
+++ hospital-be/src/HospitalLibrary/Migrations/20221213224411_map-location-changes.Designer.cs
@@ -6,15 +6,17 @@ using HospitalLibrary.Settings;
 using HospitalLibrary.Users.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace HospitalLibrary.Migrations
 {
     [DbContext(typeof(HospitalDbContext))]
-    partial class HospitalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221213224411_map-location-changes")]
+    partial class maplocationchanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
