740cabb6f59f8f59941ea7c394c56d4c5883a84f
SpasojevicAleksa <spasojevic.ra136.2019@uns.ac.rs>
2022-12-15 19:21:02 +0100

    MapItem Update E2E test

---
+++ hospital-be/src/TestHospitalApp/EndToEndTesting/Pages/Maps/BuildingMapPage.cs
@@ -0,0 +1,82 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHospitalApp.EndToEndTesting.Pages.Maps
+{
+    public class BuildingMapPage
+    {
+        private readonly IWebDriver driver;
+        public const string URI = @"http://localhost:4200/manager/maps";
+        public const string LoginURI = @"http://localhost:4200/login";
+
+        public BuildingMapPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private IWebElement buildingB => driver.FindElement(By.XPath("//*[@id='52d812c9-ab26-46ba-a4f6-17f492c71510']"));
+
+        private IWebElement submitB => driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-edit-building/div/button"));
+
+        private IWebElement tempButton => driver.FindElement(By.XPath("/html/body/app-root/app-manager-root/app-maps-main-container/div/div[2]/div[1]/app-buildings/div/div[2]/div/div/mat-card/button"));
+
+        private IWebElement tempBox => driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-edit-building/div/mat-form-field[2]/div/div[1]/div/input"));
+
+        public void press()
+        {
+            WebDriverWait myDynamicElement = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            myDynamicElement.Until(driver => CheckIfItExists("//*[@id='52d812c9-ab26-46ba-a4f6-17f492c71510']"));
+            buildingB.Click();
+        }
+        public bool CheckIfItExists(string ElementId)
+        {
+            try
+            {
+                driver.FindElement(By.XPath(ElementId));
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        public void press3()
+        {
+            WebDriverWait myDynamicElement = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            myDynamicElement.Until(driver => CheckIfItExists("/html/body/app-root/app-manager-root/app-maps-main-container/div/div[2]/div[1]/app-buildings/div/div[2]/div/div/mat-card/button"));
+            tempButton.Click();
+        }
+
+        public void write(String s)
+        {
+            WebDriverWait myDynamicElement = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            myDynamicElement.Until(driver => CheckIfItExists("/html/body/div[2]/div[2]/div/mat-dialog-container/app-edit-building/div/mat-form-field[2]/div/div[1]/div/input"));
+            tempBox.Clear();
+            tempBox.SendKeys(s);
+        }
+
+        public void press2()
+        {
+            WebDriverWait myDynamicElement = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            myDynamicElement.Until(driver => CheckIfItExists("/html/body/div[2]/div[2]/div/mat-dialog-container/app-edit-building/div/button"));
+            submitB.Click();
+        }
+
+        public String read()
+        {
+            WebDriverWait myDynamicElement = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            myDynamicElement.Until(driver => CheckIfItExists("/html/body/div[2]/div[2]/div/mat-dialog-container/app-edit-building/div/mat-form-field[2]/div/div[1]/div/input"));
+            String s = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-edit-building/div/mat-form-field[2]/div/div[1]/div/input")).GetAttribute("value").ToString();
+            return s;
+        }
+
+        public void Navigate() => driver.Navigate().GoToUrl(URI);
+        public void NavigateStart() => driver.Navigate().GoToUrl(LoginURI);
+    }
+}
---
+++ hospital-be/src/TestHospitalApp/EndToEndTesting/Tests/UpdateMapItem/UpdateBuildingTest.cs
@@ -0,0 +1,77 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHospitalApp.EndToEndTesting.Pages.Feedback;
+using TestHospitalApp.EndToEndTesting.Pages.Maps;
+using Xunit;
+
+namespace TestHospitalApp.EndToEndTesting.Tests.UpdateMapItem
+{
+    public class UpdateBuildingTest
+    {
+
+        public IWebDriver Driver;
+        public BuildingMapPage mapPage;
+        private LoginPageManager loginPage;
+        public int rowCount;
+
+        public UpdateBuildingTest()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");            // open Browser in maximized mode
+            options.AddArguments("disable-infobars");           // disabling infobars
+            options.AddArguments("--disable-extensions");       // disabling extensions
+            options.AddArguments("--disable-gpu");              // applicable to windows os only
+            options.AddArguments("--disable-dev-shm-usage");    // overcome limited resource problems
+            options.AddArguments("--no-sandbox");               // Bypass OS security model
+            options.AddArguments("--disable-notifications");    // disable notifications
+
+            LoginPrivate(options);
+
+            MapNavigate(options);
+        }
+
+        public void Dispose()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+
+        private void LoginPrivate(ChromeOptions options)
+        {
+            Driver = new ChromeDriver(options);
+            mapPage = new BuildingMapPage(Driver);
+            loginPage = new LoginPageManager(Driver);
+            mapPage.NavigateStart();
+            loginPage.EnterUsernameAndPassword("manager1", "manager1");
+            loginPage.PressLoginButton();
+
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => driver.Url == "http://localhost:4200/manager");
+        }
+
+        private void MapNavigate(ChromeOptions options)
+        {
+            mapPage.Navigate();
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => driver.Url == "http://localhost:4200/manager/maps");
+        }
+
+        [Fact]
+        public void UpdateBuilding()
+        {
+            String newName = "Zgrada BB";
+            mapPage.press();
+            mapPage.press3();
+            mapPage.write(newName);
+            mapPage.press2();
+            mapPage.press3();
+            Assert.Equal(newName, mapPage.read());
+        }
+    }
+}
