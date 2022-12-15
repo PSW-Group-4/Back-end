bd11535f69b9aaee18f5012ab43241aad3d0513e
Bojana Karanovic <bojana.karanovic2015@gmail.com>
2022-12-15 10:38:18 +0100

    E2E test for cancel move equipment appointment

--- hospital-be/src/TestHospitalApp/EndToEndTesting/Pages/CancelAppointment/CancelAppointmentPage.cs
+++ hospital-be/src/TestHospitalApp/EndToEndTesting/Pages/CancelAppointment/CancelAppointmentPage.cs
@@ -1,6 +1,9 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +13,151 @@ namespace TestHospitalApp.EndToEndTesting.Pages.CancelAppointment
     public class CancelAppointmentPage
     {
         private readonly IWebDriver driver;
-        public const string URI = @"http://localhost:4200/manager/consiliums";
+        public const string URI = @"http://localhost:4200/manager/maps";
         public const string LoginURI = @"http://localhost:4200/login";
+        private IWebElement Table => driver.FindElement(By.Id("searchNameTable"));
+
+        private ReadOnlyCollection<IWebElement> Rows =>
+            Table.FindElements(By.TagName("tr"));
+        private IWebElement divTest => driver.FindElement(By.Id("divDialog"));
+        private IWebElement tr => driver.FindElement(By.XPath("/html/body/app-root/app-manager-root/app-maps-main-container/div/div[2]/div[2]/app-search/mat-card/mat-card-content/mat-tab-group/div/mat-tab-body[1]/div/table/tbody/tr[1]"));
+        private IWebElement showRoomScheduleButton => divTest.FindElement(By.XPath("/html/body/app-root/app-manager-root/app-maps-main-container/div/div[2]/div[1]/app-rooms/div/div[3]/div/div/mat-card/button[4]"));
+
+        private IWebElement equipmentRelocationTab => driver.FindElement(By.Id("EquipmentRelocation"));
+        private IWebElement equipmentTable => driver.FindElement(By.Id("equipmentRelocationTable"));
+        private IWebElement cancelMoveEquipmentAppointmentButton => driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-room-schedule/div/mat-tab-group/div/mat-tab-body[1]/div/table/tbody/tr/td[3]/button"));
+
+        public CancelAppointmentPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public bool ShowRoomScheduleButtonPressed() 
+        {
+            try
+            {
+                var button = showRoomScheduleButton;
+                button.Click();
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool TrPressed()
+        {
+            try
+            {
+                var trr = tr;
+                trr.Click();
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+            return true;
+
+        }
+        public bool DivSelected()
+        {
+            try
+            {
+                var trr = divTest;
+                trr.Click();
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool CancelMoveEquipmentAppointmentButtonPressed()
+        {
+            try
+            {
+                var button = cancelMoveEquipmentAppointmentButton;
+                button.Click();
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool EquipmentRelocationTabPressed()
+        {
+            try
+            {
+                var tab = equipmentRelocationTab;
+                tab.Click();
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool EquipmentRelocationTablePressed()
+        {
+            try
+            {
+                var tab = equipmentTable;
+                tab.Click();
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void clickOutside()
+        {
+            Actions action = new Actions(driver);
+            action.MoveByOffset(0, 0).Click().Build().Perform();
+        }
+
+        public int GetRowsCount()
+        {
+            return Rows.Count;
+        }
+
+        public void EnsurePageIsDisplayed()
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return Table.Displayed;
+                }
+                
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public void EnsureEndPageIsDisplayed()
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return equipmentRelocationTab.Displayed;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public void Navigate() => driver.Navigate().GoToUrl(URI);
+        public void NavigateStart() => driver.Navigate().GoToUrl(LoginURI);
+        
     }
 }
---
+++ hospital-be/src/TestHospitalApp/EndToEndTesting/Tests/CancelAppointment/CreateCancelAppointmentTest.cs
@@ -0,0 +1,78 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHospitalApp.EndToEndTesting.Pages.CancelAppointment;
+using TestHospitalApp.EndToEndTesting.Pages.Consilium;
+using TestHospitalApp.EndToEndTesting.Pages.Login;
+using Xunit;
+
+namespace TestHospitalApp.EndToEndTesting.Tests.CancelAppointment
+{
+    public class CreateCancelAppointmentTest
+    {
+        public IWebDriver Driver;
+        public CancelAppointmentPage CancelAppointmentPage;
+        private LoginPage loginPage;
+        public int rowCount;
+
+        public CreateCancelAppointmentTest() {
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");            // open Browser in maximized mode
+            options.AddArguments("disable-infobars");           // disabling infobars
+            options.AddArguments("--disable-extensions");       // disabling extensions
+            options.AddArguments("--disable-gpu");              // applicable to windows os only
+            options.AddArguments("--disable-dev-shm-usage");    // overcome limited resource problems
+            options.AddArguments("--no-sandbox");               // Bypass OS security model
+            options.AddArguments("--disable-notifications");    // disable notifications
+
+            LoginPrivate(options);
+
+            CancelAppointmentPage.Navigate();
+        }
+
+        private void LoginPrivate(ChromeOptions options)
+        {
+            Driver = new ChromeDriver(options);
+            CancelAppointmentPage = new CancelAppointmentPage(Driver);
+            loginPage = new LoginPage(Driver);
+            CancelAppointmentPage.NavigateStart();
+            loginPage.EnterUsernameAndPassword("manager1", "manager1");
+            loginPage.PressLoginButton();
+
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => driver.Url == "http://localhost:4200/manager");
+        }
+
+        [Fact]
+        public void Cancel_move_equipment_appointment()
+        {
+            CancelAppointmentPage.EnsurePageIsDisplayed();
+            
+            rowCount = CancelAppointmentPage.GetRowsCount();
+            //CancelAppointmentPage.TrPressed();
+            
+            Assert.True(CancelAppointmentPage.TrPressed());
+            //CancelAppointmentPage.NavigateRoom();
+            //CancelAppointmentPage.DivSelected();
+            //Assert.True(CancelAppointmentPage.DivSelected());
+            CancelAppointmentPage.ShowRoomScheduleButtonPressed();
+            //Assert.True(CancelAppointmentPage.ShowRoomScheduleButtonPressed());
+            //CancelAppointmentPage.EnsureEndPageIsDisplayed();
+            CancelAppointmentPage.EquipmentRelocationTabPressed();
+            CancelAppointmentPage.EquipmentRelocationTablePressed();
+            CancelAppointmentPage.CancelMoveEquipmentAppointmentButtonPressed();
+
+            //
+            //Assert.True(CancelAppointmentPage.CancelMoveEquipmentAppointmentButtonPressed());
+            //Assert.True(CancelAppointmentPage.DivSelected());
+
+            //Assert.Equal(rowCount, CancelAppointmentPage.GetRowsCount());
+        }
+    }
+}
