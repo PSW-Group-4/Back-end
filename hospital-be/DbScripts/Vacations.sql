0aef163e7dec6fa9533d1051fc938b751c9aeb13
DusanVukovic3 <vukovic.ra16.2019@uns.ac.rs>
2022-12-15 14:44:19 +0100

    dbResetScript_VacationsAdded

---
+++ hospital-be/DbScripts/Vacations.sql
@@ -0,0 +1,8 @@
+insert into "Vacations" ("Id", "DoctorId", "DateStart", "DateEnd", "Reason", "Urgent", "VacationStatus", "DeniedRequestReason") 
+		values 	('030d8222-259f-4995-899c-fc6aa7d6a053','6444dff2-fcd7-44b0-90cb-19326b7ad49d', '2022-11-04 00:00:00', '2022-11-09 00:00:00', 'Zenim se', true, 2, 'Ne moze ove nedelje'),
+		   	('54cacecd-6d49-40cb-ad60-d0b95bcbf7ba','6444dff2-fcd7-44b0-90cb-19326b7ad49d', '2022-12-20 00:00:00', '2022-12-28 00:00:00', 'Zenim se drugi put', true, 1, ''),
+			('e5c8afe0-8ebd-4509-8b66-26f9e19533c0','6444dff2-fcd7-44b0-90cb-19326b7ad49d', '2022-05-28 00:00:00', '2022-06-07 00:00:00', 'Ne mogu da radim mrzi me', false, 0, ''),
+			('96453600-20e5-4fbf-bf75-a8225eda4116','6444dff2-fcd7-44b0-90cb-19326b7ad49d', '2022-01-01 00:00:00', '2022-02-05 00:00:00', 'Umoran sam', false, 1, ''),
+			('a0d68360-9ead-4acc-927c-e2444fc2e66b','6444dff2-fcd7-44b0-90cb-19326b7ad49d', '2022-03-20 00:00:00', '2022-04-22 00:00:00', 'Boli me prst na nozi', false, 1, ''),
+			('c512766c-21bc-4240-ac4c-e3a2a1946bb4','6444dff2-fcd7-44b0-90cb-19326b7ad49d', '2022-06-20 00:00:00', '2022-06-21 00:00:00', 'Finale svetskog prvenstva', true, 1, ''),
+			('887bc79b-fc88-4b44-b86f-e6998f1ee7d4','6444dff2-fcd7-44b0-90cb-19326b7ad49d', '2022-07-20 00:00:00', '2022-07-31 00:00:00', 'Razvodim se', false, 2, 'Jer brakovi ne traju kao sto su nekad')
