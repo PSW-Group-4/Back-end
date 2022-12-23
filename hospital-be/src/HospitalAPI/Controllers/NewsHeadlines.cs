a48ebb68de550e8d4be05fede3b790062568f648
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-12-23 14:37:12 +0100

    News headlines controller

---
+++ hospital-be/src/HospitalAPI/Controllers/NewsHeadlines.cs
@@ -0,0 +1,26 @@
+﻿using HospitalAPI.Dtos.NewsHeadlines;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace HospitalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsHeadlines : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            List<NewsHeadlinesDTO> list = new List<NewsHeadlinesDTO>();
+            list.Add(new NewsHeadlinesDTO("Vest 1"));
+            list.Add(new NewsHeadlinesDTO("Vest 2"));
+            list.Add(new NewsHeadlinesDTO("Vest 3"));
+            list.Add(new NewsHeadlinesDTO("Vest 4"));
+            list.Add(new NewsHeadlinesDTO("Vest 5"));
+            list.Add(new NewsHeadlinesDTO("Vest 6"));
+            list.Add(new NewsHeadlinesDTO("Vest 7"));
+            return Ok(list);
+        }
+
+    }
+}
---
+++ hospital-be/src/HospitalAPI/Dtos/NewsHeadlines/NewsHeadlinesDTO.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalAPI.Dtos.NewsHeadlines
+{
+    public class NewsHeadlinesDTO 
+    {
+        public string Headline { get; set; }
+
+        public NewsHeadlinesDTO(string headline)
+        {
+            Headline = headline;
+        }
+    }
+
+}
