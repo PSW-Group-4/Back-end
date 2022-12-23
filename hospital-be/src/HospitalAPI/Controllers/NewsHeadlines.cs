23a0c94870e531c12857fa009aff67ca82bef67f
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-12-23 16:07:06 +0100

    Update NewsHeadlines.cs

--- hospital-be/src/HospitalAPI/Controllers/NewsHeadlines.cs
+++ hospital-be/src/HospitalAPI/Controllers/NewsHeadlines.cs
@@ -12,25 +12,21 @@ namespace HospitalAPI.Controllers
         public ActionResult GetAll()
         {
             List<NewsHeadlinesDTO> list = new List<NewsHeadlinesDTO>();
-            list.Add(new NewsHeadlinesDTO("Vest 1 backend"));
-            list.Add(new NewsHeadlinesDTO("Vest 2"));
-            list.Add(new NewsHeadlinesDTO("Vest 3"));
-            list.Add(new NewsHeadlinesDTO("Vest 4"));
-            list.Add(new NewsHeadlinesDTO("Vest 5"));
-            list.Add(new NewsHeadlinesDTO("Vest 6"));
-            list.Add(new NewsHeadlinesDTO("Vest 7"));
-            list.Add(new NewsHeadlinesDTO("Vest 2"));
-            list.Add(new NewsHeadlinesDTO("Vest 3"));
-            list.Add(new NewsHeadlinesDTO("Vest 4"));
-            list.Add(new NewsHeadlinesDTO("Vest 5"));
-            list.Add(new NewsHeadlinesDTO("Vest 6"));
-            list.Add(new NewsHeadlinesDTO("Vest 7"));
-            list.Add(new NewsHeadlinesDTO("Vest 2"));
-            list.Add(new NewsHeadlinesDTO("Vest 3"));
-            list.Add(new NewsHeadlinesDTO("Vest 4"));
-            list.Add(new NewsHeadlinesDTO("Vest 5"));
-            list.Add(new NewsHeadlinesDTO("Vest 6"));
-            list.Add(new NewsHeadlinesDTO("Vest 7"));
+            list.Add(new NewsHeadlinesDTO("Vest: Jana kida Jana kida Jana kida Jana kida Jana kida"));
+            list.Add(new NewsHeadlinesDTO("Vest: Dunja dominira"));
+            list.Add(new NewsHeadlinesDTO("Vest: Jovana gori"));
+            list.Add(new NewsHeadlinesDTO("Vest: Jovan je car"));
+
+            list.Add(new NewsHeadlinesDTO("Vest: Jana kida"));
+            list.Add(new NewsHeadlinesDTO("Vest: Dunja dominira"));
+            list.Add(new NewsHeadlinesDTO("Vest: Jovana gori"));
+            list.Add(new NewsHeadlinesDTO("Vest: Jovan je car"));
+
+            list.Add(new NewsHeadlinesDTO("Vest: Jana kida"));
+            list.Add(new NewsHeadlinesDTO("Vest: Dunja dominira"));
+            list.Add(new NewsHeadlinesDTO("Vest: Jovana gori"));
+            list.Add(new NewsHeadlinesDTO("Vest: Jovan je car"));
+
             return Ok(list);
         }
 
