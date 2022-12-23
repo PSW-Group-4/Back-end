aff841b05f709588b770a9d6301ff6d5c3b880a4
strale15 <karte32114@gmail.com>
2022-12-23 10:15:52 +0100

    Agregate root

---
+++ hospital-be/src/HospitalLibrary/Core/EventSourcingCore/EventSourcedAggregate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Core.EventSourcingCore
+{
+    public abstract class EventSourcedAggregate
+    {
+        public Guid Id { get; protected set; }
+        public List<DomainEvent> Changes { get; private set; }
+        public int Version { get; protected set; }
+
+        public EventSourcedAggregate()
+        {
+            Changes = new List<DomainEvent>();
+        }
+
+        public abstract void Apply(DomainEvent changes);
+    }
+}
