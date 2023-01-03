eb9a2f99a814e98fa75f28ebac26facf73da74f1
Sanja Petrović <sanjica.petrovic7@gmail.com>
2023-01-03 16:29:05 +0100

    Implement basic DDD classes

---
+++ hospital-be/src/IntegrationLibrary/EventSourcing/DomainEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IntegrationLibrary.EventSourcing
+{
+    public abstract class DomainEvent
+    {
+        public Guid Id { get; private set; }
+        public Guid AggregateId { get; private set; }
+        public DateTime Timestamp { get; private set; }
+        
+        
+        public DomainEvent(Guid aggregateId, DateTime timestamp)
+        {
+            AggregateId = aggregateId;
+            Timestamp = timestamp;
+        }
+
+    }
+}
---
+++ hospital-be/src/IntegrationLibrary/EventSourcing/IEventRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace IntegrationLibrary.EventSourcing
+{
+    public interface IEventRepository<TEvent, TAggregateRoot>
+    {
+        public void SaveEvent(TEvent @event);
+        public  TAggregateRoot GetById(Guid id);
+    }
+}
