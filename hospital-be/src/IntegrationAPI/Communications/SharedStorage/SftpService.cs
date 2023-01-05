17ed4da49ce589d9e55b51f5a67c20c1282a34a4
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2023-01-05 23:42:34 +0100

    Feature/shared storage (#186)
    * Set up SFTP config and add some tests
    
    * feat: implement connection and upload to rebex server
    
    * feat: implement download file from server

---
+++ hospital-be/src/IntegrationAPI/Communications/SharedStorage/SftpService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+
+using Rebex.Net;
+
+namespace IntegrationAPI.Communications.SharedStorage
+{
+    public class SftpService
+    {
+        private readonly SftpConfig _config;
+        private readonly Sftp _client;
+
+        public SftpService(SftpConfig sftpConfig)
+        {
+            _config = sftpConfig;
+            Rebex.Licensing.Key = "==AFeeZVSzy8xk4CHcnI9hkMoxe6IW+JSJ/2DJeRjjBAuM==";
+            _client = new Rebex.Net.Sftp();
+            SetupClient();
+        }
+
+        ~SftpService()
+        {
+          _client.Disconnect();  
+        }
+
+        public void SetupClient()
+        {
+            _client.Connect(_config.Host);
+            _client.Login(_config.Username, _config.Password);
+            if (!_client.DirectoryExists("/reports"))
+                _client.CreateDirectory("/reports");
+        }
+
+        public SftpItemCollection ListAll()
+        {
+            return _client.GetList();
+        }
+
+        public void UploadFile(string localFilePath, string remoteFilePath)
+        {
+            _client.PutFile(localFilePath, remoteFilePath);
+        }
+
+        public void DownloadFile(string localFilePath, string remoteFilePath)
+        {
+            _client.GetFile(remoteFilePath, localFilePath);
+        }
+    }
+}
