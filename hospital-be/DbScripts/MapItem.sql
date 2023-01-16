b53e11cb0dce18f6252d057234e9d49bb4c91f89
MiskLG <misklg283@gmail.com>
2023-01-16 03:54:29 +0100

    Small update to that every room has a logical entry

--- hospital-be/DbScripts/MapItem.sql
+++ hospital-be/DbScripts/MapItem.sql
@@ -13,8 +13,8 @@ INSERT INTO public."MapItem"(
 			('e3d38c92-01de-4d49-afb5-90de4dad2a42', 'f65545be-c944-453d-9d41-d6463553279a', 20, 0, 45, 300, 'FloorMap')
 	
 	;
-INSERT INTO public."MapItem"("Id", "RoomId", "CoordinateX", "CoordinateY", "Height", "Width", "Discriminator") VALUES ('1dd897c6-56a0-4b9c-be76-c5cfd0ccfea0', '9ae3255d-261f-472f-a961-7f2e7d05d95c', 0, 0, 80, 80, 'RoomMap');
-INSERT INTO public."MapItem"("Id", "RoomId", "CoordinateX", "CoordinateY", "Height", "Width", "Discriminator") VALUES ('4c8b0af2-9936-46a3-9eaf-002840685f64', '8dd731ee-197f-40a8-a5e1-845662b0c0cd', 0, 80, 80, 80, 'RoomMap');
+INSERT INTO public."MapItem"("Id", "RoomId", "CoordinateX", "CoordinateY", "Height", "Width", "Discriminator") VALUES ('1dd897c6-56a0-4b9c-be76-c5cfd0ccfea0', '9ae3255d-261f-472f-a961-7f2e7d05d95c', 0, 0, 100, 80, 'RoomMap');
+INSERT INTO public."MapItem"("Id", "RoomId", "CoordinateX", "CoordinateY", "Height", "Width", "Discriminator") VALUES ('4c8b0af2-9936-46a3-9eaf-002840685f64', '8dd731ee-197f-40a8-a5e1-845662b0c0cd', 0, 100, 80, 80, 'RoomMap');
 INSERT INTO public."MapItem"("Id", "RoomId", "CoordinateX", "CoordinateY", "Height", "Width", "Discriminator") VALUES ('c2c47c06-4403-4f77-8c70-ed570e2a8812', 'd4857133-ef89-4e5e-865f-b49c83ecec23', 80, 0, 80, 80, 'RoomMap'),
 			('0f753a34-bf93-4e9a-ac80-5922a2394ae0', '424c4309-1154-4d52-9282-342601cef85c', 160, 0, 80, 80, 'RoomMap'),
 			('af16141a-8ebc-4c84-a22e-0990d0c50efe', '6b046e88-546c-4071-9431-24511e1e2d28', 240, 0, 80, 80, 'RoomMap'),
