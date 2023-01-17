1317604515d81b0e7b6c22a08b6e1fdb89214d91
Đorđe <the2922000@gmail.com>
2023-01-17 19:59:47 +0100

    Update dbResetScript.sql

--- hospital-be/dbResetScript.sql
+++ hospital-be/dbResetScript.sql
@@ -665,10 +665,10 @@ INSERT INTO public."Symptoms" ("Id","Name") VALUES
 INSERT INTO public."BloodSupply" ("Id","BloodGroup","RhFactor","Amount") VALUES
 	('b14fb18a-e0d9-460c-8dd5-043b0b87dfa4',1,0,200.00),
 	('a6ac6cbf-4d30-4c4c-9b7a-fc110433257c',1,1,450.00),
-	('4996bada-9081-41e9-9fac-79c484237b3f',2,0,0.00),
+	('4996bada-9081-41e9-9fac-79c484237b3f',2,0,500.00),
 	('986f00ae-4a94-46a8-b34b-c2a2a572ee50',2,1,875.00),
 	('91b65daf-f81b-47d4-bf15-7da65beb3eab',3,0,380.00),
-	('5c5a9b38-0d4a-4106-9e28-c9f26dbe37f4',3,1,0.00),
+	('5c5a9b38-0d4a-4106-9e28-c9f26dbe37f4',3,1,500.00),
 	('90341a02-a1e2-4622-8955-5bb8baef12b8',0,0,1150.00),
 	('52d280df-3c9f-46e7-8be2-69da3235ad26',0,1,700.00);
 -- Vise ne puca, pa sam privremeno otkomentarisao, Relocation appointments
